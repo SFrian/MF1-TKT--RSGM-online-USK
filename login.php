602bd33cf350a8ae61de41a0a2d9344cefa6cc6b
Syarif_Rian <syarif_rh@mhs.unsyiah.ac.id>
2022-04-15 01:59:00 +0700

    menambahkan variabel user

---
+++ login.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Login</title>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Login Page</h1>
+    <form action="login.php" method="post">
+
+        <label for="username">Username</label>
+        <input type="text" name="username" id="username">
+
+        <label for="password">Password</label>
+        <input type="password" name="password" id="password"
+        >
+        <label for="password2">Konfirmasi password</label>
+        <input type="password" name="password2" id="password2">
+
+        <input type="submit" value="Login">
+
+
+
+
+    </form>
+
+    
+</body>
+</html>
