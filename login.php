aef28ee568df25936a7a40aa29e124e85af1a6a7
Syarif_Rian <syarif_rh@mhs.unsyiah.ac.id>
2022-04-15 03:32:22 +0700

    memperbaiki halaman login

--- login.php
+++ login.php
@@ -1,32 +1,56 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <title>Login</title>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <h1>Login Page</h1>
-    <form action="login.php" method="post">
+<?php
+    $page_title = 'Login';
+    include_once'includes/header.php';
+    include_once'controllers/ParseLogin.php'; 
+?>
 
-        <label for="username">Username</label>
-        <input type="text" name="username" id="username">
+        <section id="login-main">
+            <div class="container">
+                <div class="row">
+                    <h1>User Authentication system.</h1>
+                    <div class="col-md-6 col-md-offset-3 col-sm-12 col-xs-12">
+                        <h3>Login</h3>
+                        <br>
 
-        <label for="password">Password</label>
-        <input type="password" name="password" id="password"
-        >
-        <label for="password2">Konfirmasi password</label>
-        <input type="password" name="password2" id="password2">
-
-        <input type="submit" value="Login">
-
-
-
-
-    </form>
-
-    
-</body>
-</html>
+                        <?php if(isset($result)){
+                            echo $result;
+                        } ?>
+                        <?php if(!empty($form_errors)){
+                            echo show_errors($form_errors);
+                        } ?>
+                    </div>
+                    <div class="col-md-6 col-md-offset-3 text-center">
+                        <form class="form-horizontal" action="" method="POST">
+                            <div class="form-group col-md-12">
+                                <div class="input-group">
+                                    <span class="input-group-addon">
+                                        <i class="fa fa-user"></i>
+                                    </span>
+                                    <input class="form-control" type="text" placeholder="username" name="username">
+                                </div>
+                            </div>
+                            <div class="form-group col-md-12">
+                                <div class="input-group">
+                                    <span class="input-group-addon">
+                                        <i class="fa fa-key"></i>
+                                    </span>
+                                    <input class="form-control" type="password" placeholder="password" name="password">
+                                </div>
+                            </div>
+                            <div class="form-group col-md-6">
+                                <div class="remember-me">
+                                    <label><input name="remember" value="yes" type="checkbox">Remember Me</label>
+                                </div>
+                            </div>
+                            <div class="form-group col-md-6 button">
+                                <input name="loginBtn" type="submit" class="btn btn-primary btn-lg pull-right" value="Login"><br />
+                            </div>
+                        </form>
+                        <div class="col-md-12 text-center lost-pwd">
+                            <a href="forgot_pwd.php">Lost your password?</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+<?php include_once'includes/footer.php'; ?>
