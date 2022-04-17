075dc26028f2428ffcc47b04c4540abf39dcad99
Silvia Roza <silviaroza@mhs.unsyiah.ac.id>
2022-04-17 23:03:42 +0700

    membuat index.php

---
+++ index.php
@@ -0,0 +1,28 @@
+<div class="jumbotron">
+	<div class="container text-center">
+		<h1>RSGM Online</h1>
+		<?php 
+            if(!isset($_SESSION['username'])):
+        ?>
+        <div id="loreg">
+            <ul class="list-inline">
+                <li>
+                    <a class="btn btn-primary btn-lg" href="login.php">Login</a>
+                </li>
+                <li>
+                    <a class="btn btn-primary btn-lg" href="register.php">Register</a>
+                </li>
+            </ul>
+        </div>
+        <?php else: ?>
+        <div id="usr">
+            <p>
+                kamu masuk sebagai $username <?php if(isset($_SESSION['username'])) echo $_SESSION['username']; ?> <a href="logout.php">Logout</a>
+            </p>
+        </div>
+        <?php endif; ?>
+        </div>
+   
+	</div>
+</div>
+<?php include_once'includes/footer.php'; ?>
