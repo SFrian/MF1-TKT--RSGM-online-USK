2a6094364c99192619ac4f6e755ec2af6be32d75
Silviaroza <99622858+Silviaroza@users.noreply.github.com>
2022-04-22 23:22:53 +0700

    update file index.php
    Menambahkan inframe panel dll

--- index.php
+++ index.php
@@ -1,35 +1,95 @@
-// Include Session
-<?php 
-    $page_title = 'Homepage';
-    include_once'includes/header.php';
-    include_once'config/Session.php';    
-?>
+<!-- blom aku tambahin formar php -->
 
-<div class="jumbotron">
-	<div class="container text-center">
-		<h1>RSGM Online</h1>
-		<?php 
-            if(!isset($_SESSION['username'])):
-        ?>
-        <div id="loreg">
-            <ul class="list-inline">
-                <li>
-                    <a class="btn btn-primary btn-lg" href="login.php">Login</a>
-                </li>
-                <li>
-                    <a class="btn btn-primary btn-lg" href="register.php">Register</a>
-                </li>
-            </ul>
+<!DOCTYPE html>
+<html lang="en">
+	
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <script
+      src="https://kit.fontawesome.com/64d58efce2.js"
+      crossorigin="anonymous"
+    ></script>
+    <link rel="stylesheet" href="style_log.css" />
+    <link rel="shortcut icon" href="img/tab.ico" type="image/x-icon">
+    <title>RSGM</title>
+  </head>
+	
+  <body>
+<!-- 	   -->
+    <div class="container">
+      <div class="forms-container">
+        <div class="signin-signup">
+          <form action="beranda.php" class="sign-in-form">
+            <h2 class="title">Sign in</h2>
+            <div class="input-field">
+              <i class="fas fa-user"></i>
+              <input type="text" placeholder="Username" />
+            </div>
+            <div class="input-field">
+              <i class="fas fa-lock"></i>
+              <input type="password" placeholder="Password" />
+            </div>
+            <input type="submit" value="Login" class="btn solid" />
+           
+            
+          </form>
+          <form action="beranda.php" class="sign-up-form">
+            <h2 class="title">Sign up</h2>
+            <div class="input-field">
+              <i class="fas fa-user"></i>
+              <input type="text" placeholder="Username" />
+            </div>
+		  
+            <div class="input-field">
+              <i class="fas fa-envelope"></i>
+              <input type="email" placeholder="Email" />
+            </div>
+            <div class="input-field">
+              <i class="fas fa-lock"></i>
+              <input type="password" placeholder="Password" />
+            </div>
+            <div>
+              <!-- <a href="beranda.html"></a> -->
+                <!-- <button  type="submit" class="btn" >Sign up</button> -->
+              <input type="submit" class="btn" value=" Sign up" />
+            
+            </div>
+           
+          </form>
         </div>
-        <?php else: ?>
-        <div id="usr">
+      </div>
+	    
+<!-- inframe  -->
+      <div class="panels-container">
+        <div class="panel left-panel">
+          <div class="content">
+            <h3>Belum punya akun ?</h3>
             <p>
-                kamu masuk sebagai $username <?php if(isset($_SESSION['username'])) echo $_SESSION['username']; ?> <a href="logout.php">Logout</a>
+              Ayo daftar sekarang dan mulai menggunakan layanan kami! 
             </p>
+            <button class="btn transparent" id="sign-up-btn"> 
+              Sign up
+            </button>
+          </div>
         </div>
-        <?php endif; ?>
+	      
+<!-- interaksi untuk aminasi css	       -->
+        <div class="panel right-panel">
+          <div class="content">
+            <h3>Sudah punya akun ?</h3>
+            <p>
+              Ayo berselancar dan mulai menggunakan layanan kami!
+            </p>
+            <button class="btn transparent" id="sign-in-btn">
+              Sign in
+            </button>
+          </div>
+        
         </div>
-   
-	</div>
-</div>
-<?php include_once'includes/footer.php'; ?>
+      </div>
+    </div>
+
+    <script src="js/app.js"></script>
+  </body>
+</html>
