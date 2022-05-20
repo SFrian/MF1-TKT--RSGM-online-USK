bce756b745ae8a2daa94a10b1f51c4693c9ad5be
Silviaroza <99622858+Silviaroza@users.noreply.github.com>
2022-05-20 13:01:46 +0700

    hubungungi kami pasien up

---
+++ hubungi_kami_pasien.php
@@ -0,0 +1,172 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Hubungi Kami</title>
+	<link rel="stylesheet" href="fontawesome/css/all.min.css"> 
+	<link href="https://fonts.googleapis.com/css2?family=Source+Sans+Pro&display=swap" rel="stylesheet"> <!-- https://fonts.google.com/ -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="css/style.css" rel="stylesheet">
+    <link rel="shortcut icon" href="img/tab.ico" type="image/x-icon">
+
+</head>
+<body>
+	<header class="tm-header" id="tm-header">
+        <div class="tm-header-wrapper">
+            <button class="navbar-toggler" type="button" aria-label="Toggle navigation">
+                <i class="fas fa-bars"></i>
+            </button>
+            
+            <div class="tm-site-header">
+                <div class="mb-3 mx-auto tm-site-logo"><i class="fas fa-times fa-2x"></i></div>            
+                <h1 class="text-center">Profile User</h1>
+            </div>
+            <nav class="tm-nav" id="tm-nav">            
+                <ul>
+                    <li class="tm-nav-item "><a href="beranda_pasien.php" class="tm-nav-link" >
+                        <i class="fas fa-home"></i>
+                        Beranda
+                    </a></li>
+                    <!-- <li class="tm-nav-item"><a href="profile_pasien.php" class="tm-nav-link">
+    
+                        <i class="fas fa-user"></i>
+                        Profile
+                    </a></li> -->
+                    <li class="tm-nav-item active"><a href="hubungi_kami_pasien.php" class="tm-nav-link">
+                        <i class="far fa-comments"></i>
+                        Hubungi Kami
+                    </a></li>
+                    
+                </ul>
+            </nav>
+
+            <div class="tm-mb-100">
+                <a rel="nofollow" href="logout.php" class="tm-social-link">
+                    <i class="fas fa-sign-out-alt tm-social-icon"></i>
+                </a>
+                <!-- <a href="https://twitter.com" class="tm-social-link">
+                    <i class="fab fa-twitter tm-social-icon"></i>
+                </a>
+                <a href="https://instagram.com" class="tm-social-link">
+                    <i class="fab fa-instagram tm-social-icon"></i>
+                </a>
+                <a href="https://linkedin.com" class="tm-social-link">
+                    <i class="fab fa-linkedin tm-social-icon"></i>
+                </a> -->
+            </div>
+           
+        </div>
+    </header>
+    <div class="container-fluid">
+        <main class="tm-main">
+            <!-- Search form -->
+            <div class="row tm-row">
+                <div class="col-12">
+                    <form method="GET" class="form-inline tm-mb-80 tm-search-form">                
+                        <!-- Ganti Sama Interaksi Baru  
+                            <input class="form-control tm-search-input" name="query" type="text" placeholder="Search..." aria-label="Search">
+                        <button class="tm-search-button" type="submit"> -->
+                            <i class="fas fa-search tm-search-icon" aria-hidden="true"></i>
+                        </button>                                
+                    </form>
+                </div>                
+            </div>            
+            <div class="row tm-row tm-mb-45">
+                <div class="col-12">    
+                    <hr class="tm-hr-primary tm-mb-55">
+                    <div class="gmap_canvas"> <!-- Google Map -->
+                        <iframe width="100%" height="477" id="gmap_canvas"
+                            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d63535.73033401628!2d95.33486724741748!3d5.569508790032199!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3040374d1cde4e4d%3A0x619061a5ed2ee9c!2sRumah%20Sakit%20Gigi%20dan%20Mulut%20Unsyiah!5e0!3m2!1sid!2sid!4v1650185496814!5m2!1sid!2sid"
+                            frameborder="0" scrolling="no" marginheight="0" marginwidth="0"></iframe>
+                            
+                    </div>
+                </div>                
+            </div>
+            <div class="row tm-row tm-mb-120">
+                <div class="col-12">
+                    <h2 class="tm-color-primary tm-post-title tm-mb-60">Hubungi Kami</h2>
+                </div>
+                <div class="col-lg-7 tm-contact-left">
+                    <form method="POST" action="" class="mb-5 ml-auto mr-0 tm-contact-form">                        
+                        <div class="form-group row mb-4">
+                            <label for="name" class="col-sm-3 col-form-label text-right tm-color-primary">Nama</label>
+                            <div class="col-sm-9">
+                                <input class="form-control mr-0 ml-auto" name="name" id="name" type="text" required placeholder="Namu ...">                            
+                            </div>
+                        </div>
+                        <div class="form-group row mb-4">
+                            <label for="email" class="col-sm-3 col-form-label text-right tm-color-primary">Email</label>
+                            <div class="col-sm-9">
+                                <input class="form-control mr-0 ml-auto" name="email" id="email" type="email" required placeholder="example@email.com ...">
+                            </div>
+                        </div>
+                        <div class="form-group row mb-4">
+                            <label for="subject" class="col-sm-3 col-form-label text-right tm-color-primary">Keluhan</label>
+                            <div class="col-sm-9">
+                                <input class="form-control mr-0 ml-auto" name="subject" id="subject" type="text" required placeholder="Tuliskan keluhan...">
+                            </div>
+                        </div>
+                        <div class="form-group row mb-5">
+                            <label for="message" class="col-sm-3 col-form-label text-right tm-color-primary" >Message</label>
+                            <div class="col-sm-9">
+                                <textarea class="form-control mr-0 ml-auto" name="message" id="message" rows="8" required ></textarea>                                
+                            </div>
+                        </div>
+                        <div class="form-group row text-right">
+                            <div class="col-12">
+                                <button class="tm-btn tm-btn-primary tm-btn-small">Kirim</button>                        
+                            </div>                            
+                        </div>                                
+                    </form>
+                </div>
+                <div class="col-lg-5 tm-contact-right">
+                    <address class="mb-4 tm-color-gray">
+                        Menerima layanan dan informasi lebih lanjut,
+                        silahkan hubungi kami di:
+                        1050-1051
+                        
+                    </address>
+                    <span class="d-block">
+                        Tel:
+                        <a href="tel:060-070-0980" class="tm-color-gray"> (0651) 755-3205</a>
+                    </span>
+                    <span class="mb-4 d-block">
+                        Email:
+                        <a href="mailto:info@company.com" class="tm-color-gray"> rsgm@unsyiah.ac.id</a>
+                    </span>
+                    <p class="mb-5 tm-line-height-short">
+                        Rumah Sakit Gigi dan Mulut(RSGM) Unsyiah  suatu pelayanan kesehatan gigi dan mulut serta sarana pendidikan bagi mahasiswa/i koas                    </p>
+                    <ul class="tm-social-links">
+                        <li class="mb-2">
+                            <a href="https://facebook.com" class="d-flex align-items-center justify-content-center">
+                                <i class="fab fa-facebook"></i>
+                            </a>
+                        </li>
+                        <li class="mb-2">
+                            <a href="https://twitter.com" class="d-flex align-items-center justify-content-center">
+                                <i class="fab fa-twitter"></i>
+                            </a>
+                        </li>
+                        <li class="mb-2">
+                            <a href="https://youtube.com" class="d-flex align-items-center justify-content-center">
+                                <i class="fab fa-youtube"></i>
+                            </a>
+                        </li>
+                        <li class="mb-2">
+                            <a href="https://instagram.com" class="d-flex align-items-center justify-content-center mr-0">
+                                <i class="fab fa-instagram"></i>
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+            </div>      
+            <footer class="row tm-row">
+                <!-- untuk footernya buat php -->
+            </footer>
+        </main>
+    </div>
+    <script src="js/jquery.min.js"></script>
+    <script src="js/templatemo-script.js"></script>
+</body>
+</html>
